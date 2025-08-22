61d624b47202464e95cea8bfd282644443c6f836
Mircea Micicoi <mircea.micicoi@opswat.com>
2025-08-22 15:16:59 +0300

    added a route to nowater teleport

--- app.py
+++ app.py
@@ -12,10 +12,16 @@ def home():
     """
     Renders the main page, passing the author's name from the environment.
     """
-    # Fetch the author name, providing a default if it's not set.
     author_name = os.environ.get("APP_AUTHOR", "Anonymous Developer")
-        
     return render_template('index.html', author=author_name)
+
+@app.route('/nowater')
+def no_water():
+    """
+    Renders the no-water teleport page.
+    """
+    author_name = os.environ.get("APP_AUTHOR", "Anonymous Developer")
+    return render_template('nowater.html', author=author_name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
